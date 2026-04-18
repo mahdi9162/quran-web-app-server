0ba1fd3e08b5366057ccd16d0bf379b10b8f5682
Mahdi Hasan <hasanmahdi6000@gmail.com>
2026-04-18 14:50:35 +0600

    fix: resolve JSON file paths for Vercel runtime

--- src/utils/readJsonFile.ts
+++ src/utils/readJsonFile.ts
@@ -2,7 +2,9 @@ import fs from 'fs';
 import path from 'path';
 
 export const readJsonFile = <T>(filePath: string): T => {
-  const fullPath = path.join(process.cwd(), filePath);
+  const fullPath = path.join(__dirname, '../../', filePath);
+
   const fileContent = fs.readFileSync(fullPath, 'utf-8');
+
   return JSON.parse(fileContent) as T;
 };
