81f304ddeac3964d2ba3e75ea7c8097840546f95
Mahdi Hasan <hasanmahdi6000@gmail.com>
2026-04-18 14:55:27 +0600

    fix: bundle quran data files and resolve json paths for vercel

--- src/utils/readJsonFile.ts
+++ src/utils/readJsonFile.ts
@@ -2,9 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export const readJsonFile = <T>(filePath: string): T => {
-  const fullPath = path.join(__dirname, '../../', filePath);
-
+  const fullPath = path.join(process.cwd(), filePath);
   const fileContent = fs.readFileSync(fullPath, 'utf-8');
-
   return JSON.parse(fileContent) as T;
 };
